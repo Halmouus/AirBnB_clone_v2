b122927634e1698bcd8b86dfc4e739169a282de4
Halmouus <javierminimaz@gmail.com>
2023-10-06 03:57:15 +0100

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that generates a .tgz archive from the contents of the
+web_static folder"""
+from fabric.api import local, task
+from datetime import datetime
+
+
+def do_pack():
+    """generates a .tgz archive from the contents
+    of the web_static folder"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(date)
+    print("Packing web_static to {}".format(path))
+    if local('{} && tar -cvzf {} web_static'.format(mkdir, path)).succeeded:
+        return path
+    return None
