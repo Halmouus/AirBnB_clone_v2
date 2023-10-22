95c91faf7ccb1c700496c9fc0be9c60bcb3f1f58
Halmouus <javierminimaz@gmail.com>
2023-10-23 00:01:51 +0100

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from flask import Flask, render_template
+from flask import Flask, render_template, app
 from models import storage
 from models.state import State
 from models.city import City
@@ -8,6 +8,7 @@ from models.city import City
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """Display a list of states and their cities"""
     states = list(storage.all(State).values())
